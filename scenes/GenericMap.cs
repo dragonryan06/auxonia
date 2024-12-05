4cc4c7e2e061f5e3e99601694319f154a18efa1e
dragonryan06 <dragonryan06@gmail.com>
2024-12-04 20:32:12 -0500

    Debug draw3d addon

--- scenes/GenericMap.cs
+++ scenes/GenericMap.cs
@@ -3,4 +3,13 @@ using System;
 
 public partial class GenericMap : CsgMesh3D
 {
+    public override void _Ready()
+    {
+        DebugDraw3D.ScopedConfig().SetThickness(0.25f);
+    }
+
+    public override void _Process(double delta)
+    {
+        DebugDraw3D.DrawBox(Vector3.Zero, Quaternion.Identity, new Vector3(100, 100, 100), Colors.Red,true,100000);
+    }
 }
